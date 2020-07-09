84f4fcf9e687e087182af3a7528d36a4b94196f3
yogicoderanne <darylanneking@gmail.com>
2020-07-09 13:11:04 -0500

    added pokedex component to create a series of pokemon & images; multiple cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+// import PokeCard from './components/PokeCard';
+import Pokedex from './components/Pokedex';
 import PokeCard from './components/PokeCard';
 
 function App() {
 	return (
 		<div className='App'>
-			<PokeCard id={4} name='Charmander' type='fire' exp={62} />
+			{/* <PokeCard id={4} name='Charmander' type='fire' exp={62} /> */}
+			<Pokedex />
 		</div>
 	);
 }
---
+++ src/components/Pokedex.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PokeCard from './PokeCard';
+
+function Pokedex(props) {
+	console.log(props.pokemon);
+	return (
+		<div className='Pokedex'>
+			<h1>Pokedex</h1>
+			{props.pokemon.map((p) => <PokeCard id={p.id} name={p.name} type={p.type} exp={p.base_exp} />)}
+		</div>
+	);
+}
+Pokedex.defaultProps = {
+	pokemon: [
+		{ id: 4, name: 'Charmander', type: 'fire', base_exp: 62 },
+
+		{ id: 7, name: 'Squirtle', type: 'water', base_exp: 63 },
+
+		{ id: 11, name: 'Metapod', type: 'bug', base_exp: 72 },
+
+		{ id: 12, name: 'Butterfree', type: 'flying', base_exp: 178 },
+
+		{ id: 25, name: 'Pikachu', type: 'electric', base_exp: 112 },
+
+		{ id: 39, name: 'Jigglypuff', type: 'normal', base_exp: 95 },
+
+		{ id: 94, name: 'Gengar', type: 'poison', base_exp: 225 },
+
+		{ id: 133, name: 'Eevee', type: 'normal', base_exp: 65 },
+	],
+};
+
+// 	return (
+// 		<div className='Pokedex'>
+// 			<h1>Pokedex</h1>
+// 			{props.pokemon.map((p) => <PokeCard id={p.id} name={p.name} type={p.type} exp={p.base_exp} />)}
+// 		</div>
+// 	);
+// }
+
+export default Pokedex;
