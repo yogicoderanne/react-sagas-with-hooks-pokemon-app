e275d0d6930eaab22010040173248c89761a7bd3
yogicoderanne <darylanneking@gmail.com>
2020-07-12 15:51:29 -0500

    added loading state

--- src/components/PokeCardWithData.js
+++ src/components/PokeCardWithData.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './PokeCard.css';
 import axios from 'axios';
 
@@ -11,25 +11,43 @@ let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 //OR can be written like: props.data.id.toString().padStart('0',3)
 
 const PokeCard = (props) => {
-	let imgSrc = `${IMAGE_SRC}${padToThree(props.id)}.png`;
-  
+  // put here 3 useState hooks for loading, error, and pokemon (initiate as null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [pokemon, setPokemon] = useState(null)  
 
   useEffect(() => {
     async function fetchData() {
       const result = await axios(`${POKE_API}${props.id}`);
       console.log(result)
       // setData(result.data.results);
+      const basePokemon = {
+        name: 'Squirtle',
+        type: 'water',
+        base_exp: 63 
+      }
+      setPokemon(basePokemon)
+      setLoading(false)
     }
     fetchData();
   }, []);
 
+  // make if statement to return <div>loading</div> if loading or error
+  if (!pokemon || loading) {
+    return <div className='loading'>loading pokemon...</div>
+  } else if (error) {
+    return <div className=''>Error</div>
+  }
 
+	let imgSrc = `${IMAGE_SRC}${padToThree(props.id)}.png`;
+
+  // change props to be from the pokemon instead
 	return (
 		<div className='PokeCard'>
-			<h1 ClassName='PokeCard-title'>{props.name}</h1>
-			<img src={imgSrc} alt='props.name' />
-			<div className='PokeCard-data'>Type:{props.type}</div>
-			<div className='PokeCard-data'>Exp:{props.exp}</div>
+			<h1 ClassName='PokeCard-title'>WithData({pokemon.name})</h1>
+			<img src={imgSrc} alt={pokemon.name} />
+			<div className='PokeCard-data'>Type:{pokemon.type}</div>
+			<div className='PokeCard-data'>Exp:{pokemon.base_exp}</div>
 		</div>
 	);
 };
